feb18ee4d3d79902dcf22b0e7e508e2ea71e4927
bjh7242 <bjh7242@rit.edu>
2017-01-16 13:44:13 -0500

    Updated add users script to include the email address LDAP attribute

--- ansible/playbooks/scripts/Add-DomainUsers.ps1
+++ ansible/playbooks/scripts/Add-DomainUsers.ps1
@@ -1,9 +1,13 @@
 $Users = Import-Csv -Path "C:\useraccounts.csv"
+
+$domainname = (Get-ADDomain | select -expand dnsroot)
+
 foreach ($User in $Users)
 {
     $Displayname = $User.mlb_name
     $SAM = $User.bref_id
     $Description = $User.mlb_team_long
     $Password = $User.password
-    New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName $SAM -Description "$Description" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -Enabled $true -ChangePasswordAtLogon $false -PasswordNeverExpires $true
+    $Email = $SAM + "@" + $domainname
+    New-ADUser -Name "$Displayname" -DisplayName "$Displayname" -SamAccountName $SAM -Description "$Description" -AccountPassword (ConvertTo-SecureString $Password -AsPlainText -Force) -EmailAddress $Email -Enabled $true -ChangePasswordAtLogon $false -PasswordNeverExpires $true
 }
