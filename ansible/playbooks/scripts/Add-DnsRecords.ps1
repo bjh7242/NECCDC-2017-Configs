9d2a7eae722f048614c2636ba18d4bc3115c4d65
bjh7242 <bjh7242@rit.edu>
2017-02-12 18:21:02 -0500

    updated DNS script to reflect the format of the blue team domain names

--- ansible/playbooks/scripts/Add-DnsRecords.ps1
+++ ansible/playbooks/scripts/Add-DnsRecords.ps1
@@ -9,25 +9,28 @@ param (
 
 Import-Csv $FileName | % {
     $CreatedRevZone = $false
-    $RevZoneName = $_.Address.split(".")[2] + "." + $_.Address.split(".")[1] + "." + $_.Address.split(".")[0] + ".in-addr.arpa"
+    $ip = $_.Address.replace("X", $TeamNumber)
+    $RevZoneName = $ip.split(".")[2] + "." + $ip.split(".")[1] + "." + $ip.split(".")[0] + ".in-addr.arpa"
 
     # If the user wants a reverse zone, create it on the first loop only
     if ($CreatePtrs -eq $true -and $CreatedRevZone -ne $true) {
-        $NetworkId = $_.Address.split(".")[0] + "." + $_.Address.split(".")[1] + "." + $_.Address.split(".")[2] + ".0/24"
+        $NetworkId = $ip.split(".")[0] + "." + $ip.split(".")[1] + "." + $ip.split(".")[2] + ".0/24"
         Add-DnsServerPrimaryZone -NetworkId "$NetworkId" -ReplicationScope Domain
         $CreatedRevZone = $true
     }
 
-    $fqdn = $_.Name.replace("teamX","team$TeamNumber")
+    $fqdn = $_.Name.replace("fantasysportsX","fantasysports$TeamNumber")
     $name = $fqdn.split(".")[0]
     $domain_split = $fqdn.split(".")
     $domain = ($domain_split[1..($domain_split.length -1)] -join ".")
-    $ip = $_.Address.replace("X", $TeamNumber)
     if ($_.Type -eq "A"){
         Add-DnsServerResourceRecordA -Name $name -IPv4Address $ip -ZoneName $domain -CreatePtr:$CreatePtrs
     }
     if ($_.Type -eq "MX"){
         Add-DnsServerResourceRecordMX -Name . -Preference 10 -MailExchange $fqdn -ZoneName $domain
+    }
+    if ($_.Type -eq "CNAME"){
+        Add-DnsServerResourceRecordCName -Name $name -HostNameAlias $_.Address -ZoneName $domain
     }
     if ($CreatePtrs -eq $true) {
         # Add-DnsServerResourceRecordPtr -name "134" -ZoneName "164.168.192.in-addr.arpa" -PtrDomainName "dc.team0.ccdc"
