1e796ce2b9ccd2205c5a569440006c42eaefc852
Chaim sanders <csanders@sparsa.org>
2017-03-13 00:27:14 -0400

    Adding registration

--- signup.php
+++ signup.php
@@ -2,10 +2,11 @@
 $( document ).ready(function() {
   $( "#add_user" ).submit(function( event ) {
     event.preventDefault();
+    var valid = "True";
     $.get( "check_username.php?uname=" + $("#username").val().trim(), function( data ) {
-	alert(data.trim());
        if(data.trim()=="invalid"){
          $("#username").addClass( "bad" );
+	 valid = "False";
        }else{
          $("#username").removeClass( "bad" );
        } 
@@ -14,15 +15,24 @@ $( document ).ready(function() {
     var re = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
     if(!re.test($("#email").val())){
 	$("#email").addClass( "bad" );
+        valid = "False";
     }else{
 	$("#email").removeClass( "bad" );
     }
     if($("#password").val() != $("#confpassword").val() | $("#password").val() == ""){
         $("#password").addClass( "bad" );
 	$("#confpassword").addClass( "bad" );
+        valid = "False";
+	
     }else{
 	$("#password").removeClass( "bad" );
         $("#confpassword").removeClass( "bad" );
+    }
+    if(valid == "True"){
+      var dat = $(this).serializeArray();
+      $.post( "add_user.php", dat, function( data ) {
+      	$("#result").html(data);
+      });
     }
   });
 });
@@ -30,11 +40,11 @@ $( document ).ready(function() {
 </script>
 
 <div id="content" style="text-align:left;">
-
+<div id="result"></div>
 <form id="add_user">
-<label for="username">Username:</label><input class="focus" id="username" type="text" placeholder="Enter Username" />
-<label for="email">Email:</label><input class="focus" id="email" type="text" placeholder="Enter Email" />
-<label for="password">Password:</label><input class="focus" id="password" type="password" placeholder="Enter Password"/>
+<label for="username">Username:</label><input class="focus" name="username" id="username" type="text" placeholder="Enter Username" />
+<label for="email">Email:</label><input class="focus" name="email" id="email" type="text" placeholder="Enter Email" />
+<label for="password">Password:</label><input class="focus" name="password" id="password" type="password" placeholder="Enter Password"/>
 <label for="confpassword">Confirm Password:</label><input class="focus" id="confpassword" type="password" placeholder="Enter Password Confirmation" />
 <input id="sub" type="submit" />
 </form>
