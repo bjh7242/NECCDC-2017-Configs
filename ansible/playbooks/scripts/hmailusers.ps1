e989c3feddde21ddecdb13e23428a3314854fcf5
bjh7242 <bjh7242@rit.edu>
2017-03-16 14:45:28 -0400

    adding some dank hmail stuff

---
+++ ansible/playbooks/scripts/hmailusers.ps1
@@ -0,0 +1,38 @@
+param([string]$dn, [string]$dnsroot)
+#$domain = Get-ADDomain 
+# 
+# The base DN for accounts (DC=example,DC=com): 
+# 
+#$dn = 'DC=f-sports1,DC=co'
+# 
+# The domain used for emails (example.com): 
+# 
+#$dnsroot = 'f-sports1.co'
+# 
+# Enter your hMailServer password here: 
+# 
+$hmail_password = "Netsys123$" 
+# 
+# Max size of account mailbox (in megs): 
+# 
+$hmail_maxsize = 10000 
+# 
+Import-Csv -Path C:\useraccounts.csv | % {
+ $hm = New-Object -ComObject hMailServer.Application 
+ $hm.Authenticate("Administrator", $hmail_password) | Out-Null 
+ $hmdom = $hm.Domains.ItemByName($dnsroot) 
+ $hmact = $hmdom.Accounts.Add() 
+ # Filling attributes for the email account based on current information, including AD integration for the password 
+ $hmact.Address = $_.bref_id + '@' + "$dnsroot"
+ $hmact.Active = $true 
+ $hmact.IsAD = $true 
+ #$hmact.MaxSize = $hmail_maxsize 
+ $hmact.ADDomain = $dn 
+ $hmact.ADUsername = $_.bref_id
+ #$hmact.PersonFirstName = $fn 
+ #$hmact.PersonLastName = $ln 
+ $hmact
+ $hmact.save() 
+ Write-Host $user.bref_id
+ Write-Host "Done." 
+}
